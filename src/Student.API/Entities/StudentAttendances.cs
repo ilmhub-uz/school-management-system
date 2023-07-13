d4efccfd5f57873e8d6b1f7e748a28f573bed2e6
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-13 20:00:28 +0500

    Student attendance entity class added

---
+++ src/Student.API/Entities/StudentAttendances.cs
@@ -0,0 +1,10 @@
+﻿namespace Student.API.Entities;
+
+public class StudentAttendances
+{
+    public Guid TopicId { get; set; }
+    public Guid StudentId { get; set; }
+    public bool Attend { get; set; }
+}
+
+}
