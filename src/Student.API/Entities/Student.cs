83b04945c5cee1685e3e3bfcde64871471fae767
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-13 20:12:08 +0500

    Student entity class added

--- src/Student.API/Entities/Student.cs
+++ src/Student.API/Entities/Student.cs
@@ -3,13 +3,13 @@
 public class Student
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string MiddleName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Username { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Username { get; set; }
     public string? PhotoUrl { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
     public Status Status { get; set; }
 }
