eb0d3364667384e77b2ac99d26d2b14e7533f4fe
Azizbek Askarov <azizasqarov773@gmail.com>
2023-07-13 21:18:58 +0500

    Name propperty added for user entity

--- src/Identity.Api/Entities/User.cs
+++ src/Identity.Api/Entities/User.cs
@@ -3,6 +3,7 @@
 public class User
 {
     public Guid Id { get; set; }
+    public required string Name { get; set; }
     public required string UserName { get; set; }
     public required string PasswordHash { get; set; }
     public DateTime CreatedAt { get; set; }
