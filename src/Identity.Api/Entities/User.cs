41594f1130436c016c754801f3abbbed20c68866
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-13 21:43:46 +0500

    User property - Password changed to PasswordHash

--- src/Identity.Api/Entities/User.cs
+++ src/Identity.Api/Entities/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public Guid Id { get; set; }
     public required string UserName { get; set; }
-    public required string Password { get; set; }
+    public required string PasswordHash { get; set; }
     public required string Name { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdateAt { get; set; }
