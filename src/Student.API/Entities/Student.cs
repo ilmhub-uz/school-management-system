90a28f2aa3dcee0a468550a6aca57a3dc6abf5bb
Azizbek Askarov <azizasqarov773@gmail.com>
2023-07-13 20:09:17 +0500

    corrected

--- src/Student.API/Entities/Student.cs
+++ src/Student.API/Entities/Student.cs
@@ -4,12 +4,12 @@ public class Student
 {
     public Guid Id { get; set; }
     public string? FirstName { get; set; }
-    public string? LastName { get; set;}
+    public string? LastName { get; set; }
     public string? MiddleName { get; set; }
     public required string PhoneNumber { get; set; }
     public required string UserName { get; set; }
-    public string? PhotoUrl { get; set; }
-    public DateTime CreateDateTime=> DateTime.Now;
-    public DateTime? UpdateTime { get; set; }
+    public string? AvatarPath { get; set; }
+    public DateTime CreatedAt => DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
     public EStudentStatus Status { get; set; }
 }
