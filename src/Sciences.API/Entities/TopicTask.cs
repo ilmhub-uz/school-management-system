3cf1d4cbe8a67e793dae07a644f3f00b5ed5986e
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-13 20:23:10 +0500

    TopicTask class added

--- src/Sciences.API/Entities/TopicTask.cs
+++ src/Sciences.API/Entities/TopicTask.cs
@@ -1,6 +1,14 @@
-﻿namespace Sciences.API.Entities
+﻿namespace Sciences.API.Entities;
+
+public class TopicTask
 {
-    public class TopicTask
-    {
-    }
+    public required Guid Id { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public required string Title { get; set; }
+    public string? Content { get; set; }
+    public string? Description { get; set; }
+    public required Topic Topic { get; set; }
+    public required Guid TopicId { get; set; }
+
 }
