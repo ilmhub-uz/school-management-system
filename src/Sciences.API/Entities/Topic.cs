a844a77f8c71ecc772bb82114a733257274dfa45
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-13 20:07:13 +0500

    add topics table

---
+++ src/Sciences.API/Entities/Topic.cs
@@ -0,0 +1,13 @@
+﻿namespace Sciences.API.Entities
+{
+    public class Topic
+    {
+            public Guid Id { get; set; }
+            public required string Name { get; set; }
+            public Guid ScienceId { get; set; }
+            public DateTime Created_At { get; set; } = DateTime.Now;
+            public DateTime? Updated_At { get; set; }
+            public DateTime? Date { get; set; }
+            public string Title { get; set; }
+    }
+}
