0ca1752b4c32f597f25bf5e40e34522aecfa122d
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-13 20:03:26 +0500

    barcha serviceslar extension papkasiga olib o'tildi

---
+++ src/Identity.Api/Extensions/AddJwtExtension.cs
@@ -0,0 +1,32 @@
+﻿using Identity.Api.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity.Api.Extensions;
+
+public static class AddJwtExtension
+{
+    public static void AddJwtValidation(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOption>(configuration.GetSection("JwtBearer"));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var signingKey = System.Text.Encoding.UTF32.GetBytes(configuration["JwtBearer:SigningKey"]!);
+
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = configuration["JwtBearer:ValidIssuer"],
+                    ValidAudience = configuration["JwtBearer:ValidAudience"],
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(signingKey),
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+    }
+
+}
