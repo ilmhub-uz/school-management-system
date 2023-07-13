32e18b4020e8e461e36035a08447747649b0b1d2
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-13 21:18:09 +0500

    Science model added

---
+++ src/Sciences.API/Models/ScienceModels/CreateScienceModel.cs
@@ -0,0 +1,12 @@
+﻿using Sciences.API.Entities;
+
+namespace Sciences.API.Models.ScienceModels;
+
+public class CreateScienceModel
+{
+    public required string Name { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
