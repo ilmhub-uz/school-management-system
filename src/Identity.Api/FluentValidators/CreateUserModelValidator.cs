adaf9a9435dcbd9319b6850cb6aa2e259482545b
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-13 19:47:25 +0500

    add-fluent-validator for CreateUserModel

---
+++ src/Identity.Api/FluentValidators/CreateUserModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Identity.Api.Models;
+
+namespace Identity.Api.FluentValidators;
+
+public class CreateUserModelValidator : AbstractValidator<CreateUserModel>
+{
+    public CreateUserModelValidator()
+    {
+        RuleFor(u => u.Email).EmailAddress();
+        RuleFor(u => u.Name).NotNull();
+        RuleFor(u => u.Password).NotNull();
+        RuleFor(u => u.UserName).NotNull()
+            .MinimumLength(1).MaximumLength(50);
+
+        /*When(condition, () =>
+        {
+            RuleFor(u => u.Name).NotNull();
+
+        }).Otherwise(() =>
+        {
+            RuleFor(u => u.Name).Null();
+        });*/
+    }
+}
