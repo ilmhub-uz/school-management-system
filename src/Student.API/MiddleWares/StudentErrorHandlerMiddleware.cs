45b816663a97682bdb2cc632d52d5e41f0b406e8
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-14 07:37:01 +0500

    add middleware to science and student project

---
+++ src/Student.API/MiddleWares/StudentErrorHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace Student.API.MiddleWares;
+
+public class StudentErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<StudentErrorHandlerMiddleware> _logger;
+
+    public StudentErrorHandlerMiddleware(RequestDelegate next, ILogger<StudentErrorHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Student internal server error!");
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(new { Error = e.Message });
+        }
+    }
+}
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseStudentErrorHandlerMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<StudentErrorHandlerMiddleware>();
+    }
+}
