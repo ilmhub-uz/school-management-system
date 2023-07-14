8ec7fe87ebd9e0d60942b602456974d4a54a9fad
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-14 15:49:28 +0500

    add-topic-table-configuration

---
+++ src/Sciences.API/Context/Congiguration/TopicConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sciences.API.Entities;
+
+namespace Sciences.API.Context.Congiguration
+{
+    public class TopicConfiguration
+    {
+        public static void Configure(EntityTypeBuilder<Topic> builder)
+        {
+            builder.ToTable("science");
+            builder.HasKey(x => x.Id);
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasColumnName("name")
+                .HasMaxLength(50);
+            builder.Property(s => s.CreatedAt).IsRequired();
+            builder.Property(s => s.Title)
+                .IsRequired()
+                .HasColumnName("title")
+                .HasMaxLength(50);
+
+
+        }
+    }
+}
