f916ea29f64aff003ae65d1716b669cb476f2191
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-14 17:49:25 +0500

    in user entity class deleted property of name

--- src/Identity.Api/Entities/User.cs
+++ src/Identity.Api/Entities/User.cs
@@ -5,7 +5,6 @@ public class User
     public Guid Id { get; set; }
     public required string UserName { get; set; }
     public required string PasswordHash { get; set; }
-    public required string Name { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdateAt { get; set; }
     public Status Status { get; set; }
