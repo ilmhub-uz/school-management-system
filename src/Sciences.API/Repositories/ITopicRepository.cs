33a12735f6b01daec62c9b59adb210625b8d8f33
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-14 20:27:14 +0500

    Updated a little bit

--- src/Sciences.API/Repositories/ITopicRepository.cs
+++ src/Sciences.API/Repositories/ITopicRepository.cs
@@ -7,7 +7,7 @@ namespace Sciences.API.Repositories;
 public interface ITopicRepository
 {
     Task<List<Topic>> GetTopics();
-    Task <CreateTopicModel> AddTopic(Guid scienceId,CreateTopicModel model);
+    Task <TopicModel> AddTopic(Guid scienceId,CreateTopicModel model);
     Task UpdateTopic(Guid scienceId, Guid topicId, UpdateTopicModel model);
     Task DeleteTopic(Guid scienceId,Guid topicId);
     Task<Topic> GetTopicById(Guid scienceId,Guid topicId);
