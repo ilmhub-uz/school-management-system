52de31be3606a0935be58b5c91b977f2f5d59040
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-14 14:36:18 +0500

    add CreateStudentModel

---
+++ src/Student.API/Models/CreateStudentModel/CreateStudentModel.cs
@@ -0,0 +1,16 @@
+﻿using Student.API.Entities;
+
+namespace Student.API.Models.CreateStudentModel;
+
+public class CreateStudentModel
+{
+    public IFormFile? File { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public required string Username { get; set; }
+    public required string PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+    public Status Status { get; set; }
+}
