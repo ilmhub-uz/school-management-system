9e5ec9c947eac835643542f65d4435a62f1c92cd
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-14 19:58:53 +0500

    ITopicRepository added

--- src/Sciences.API/Repositories/ITopicRepository.cs
+++ src/Sciences.API/Repositories/ITopicRepository.cs
@@ -6,5 +6,9 @@ namespace Sciences.API.Repositories;
 
 public interface ITopicRepository
 {
-
+    Task<List<Topic>> GetTopics();
+    Task AddTopic(CreateTopicModel model);
+    Task UpdateTopic(Guid scienceId, UpdateTopicModel model);
+    Task DeleteTopic(Guid topicId);
+    Task<Science> GetTopicById(Guid topicId);
 }
