a83c8d83d62b6fd63065e0ef1b8a50bb84bb02ad
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-14 19:03:51 +0500

    add CreateStudentModelValidator

---
+++ src/Student.API/FluentValidators/CreateStudentModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Student.API.Models.StudentModels;
+
+namespace Student.API.FluentValidators;
+
+public class CreateStudentModelValidator : AbstractValidator<CreateStudentModel>
+{
+    public CreateStudentModelValidator()
+    {
+        
+    }
+}
