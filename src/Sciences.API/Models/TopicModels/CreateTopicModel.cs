90d2f9abf6fa86a4c399c85e832170e5b6d03f5b
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-14 17:41:35 +0500

    adding CreatedTopicModel

--- src/Sciences.API/Models/TopicModels/CreateTopicModel.cs
+++ src/Sciences.API/Models/TopicModels/CreateTopicModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Sciences.API.Models.TopicModels
+﻿using Sciences.API.Entities;
+
+namespace Sciences.API.Models.TopicModels;
+
+public class CreateTopicModel
 {
-    public class CreateTopicModel
-    {
-    }
+    public required string Name { get; set; }
+    public required string Title { get; set; }
 }
