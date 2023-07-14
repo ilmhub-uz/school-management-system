68ef8676b6c5b1e13b372096c09764e2bfc0cf77
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-14 17:25:26 +0500

    UpdateScienceModel added

--- src/Sciences.API/Models/ScienceModels/UpdateScienceModel.cs
+++ src/Sciences.API/Models/ScienceModels/UpdateScienceModel.cs
@@ -1,6 +1,8 @@
-﻿namespace Sciences.API.Models.ScienceModels
+﻿namespace Sciences.API.Models.ScienceModels;
+
+public class UpdateScienceModel
 {
-    public class UpdateScienceModel
-    {
-    }
+    public required string Name { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
 }
