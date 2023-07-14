bd73544a95d0c0ac2d58442abaa757e21e87121d
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-14 19:09:03 +0500

    change CreateStudentModelValidator

--- src/Student.API/FluentValidators/CreateStudentModelValidator.cs
+++ src/Student.API/FluentValidators/CreateStudentModelValidator.cs
@@ -7,6 +7,15 @@ public class CreateStudentModelValidator : AbstractValidator<CreateStudentModel>
 {
     public CreateStudentModelValidator()
     {
-        
+        RuleFor(r => r.FirstName)
+            .MaximumLength(100).MinimumLength(1);
+        RuleFor(r => r.LastName)
+            .MaximumLength(100).MinimumLength(1);
+        RuleFor(r => r.MiddleName)
+            .MaximumLength(100).MinimumLength(1);
+        RuleFor(r => r.Username)
+            .NotNull().MaximumLength(100).MinimumLength(1);
+        RuleFor(r => r.PhoneNumber)
+            .NotNull().MaximumLength(100).MinimumLength(1);
     }
 }
