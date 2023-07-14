adaeb483c6b72484033ee0a6a56ebee89231926f
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-14 15:31:11 +0500

    sciences-table-configuration-added

---
+++ src/Sciences.API/Context/Congiguration/ScienceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sciences.API.Entities;
+
+namespace Sciences.API.Context.Congiguration
+{
+    public class ScienceConfiguration
+    {
+        public static void Configure(EntityTypeBuilder<Science> builder)
+        {
+            builder.ToTable("science");
+            builder.HasKey(x => x.Id);
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasColumnName("name")
+                .HasMaxLength(50);
+            builder.Property(s => s.CreatedAt).IsRequired();
+            builder.Property(s => s.Title)
+                .IsRequired()
+                .HasColumnName("title")
+                .HasMaxLength(50);
+
+
+        }
+    }
+}
