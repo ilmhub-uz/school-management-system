6f547957b58afb4b25f4478f2e29e159161264ac
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-14 17:23:55 +0500

    Created Science model

--- src/Sciences.API/Models/ScienceModels/CreateScienceModel.cs
+++ src/Sciences.API/Models/ScienceModels/CreateScienceModel.cs
@@ -5,6 +5,5 @@ public class CreateScienceModel
     public required string Name { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
-    public required DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+
 }
