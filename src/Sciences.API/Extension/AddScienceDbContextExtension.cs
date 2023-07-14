e93b9b6bcb4e6da7f414f1523eacec7ed1dcdc60
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-14 10:41:05 +0500

    add extension folder to science project

---
+++ src/Sciences.API/Extension/AddScienceDbContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sciences.API.Context;
+
+namespace Sciences.API.Extension;
+
+public static class AddScienceDbContextExtension
+{
+    public static void AddScienceDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        services.AddDbContext<ScienceDbContext>(config =>
+        {
+            config.UseNpgsql(configuration.GetConnectionString("ScienceDb"));
+        });
+    }
+}
---
+++ src/Sciences.API/Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Sciences.API.Extension;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddScienceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddScienceDbContext(configuration);
+    }
+}
