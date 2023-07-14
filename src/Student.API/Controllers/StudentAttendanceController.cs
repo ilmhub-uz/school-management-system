4e38195b751ee2d4e535bec48315edcc15d62146
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-14 04:24:23 -0700

    StudentAttendance controller with all end points were added

---
+++ src/Student.API/Controllers/StudentAttendanceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student.API.Controllers;
+
+[Route("api/students/{username}/attendances")]
+[ApiController]
+public class StudentAttendanceController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAttendances(string username)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAttendance()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{attendanceId}")]
+    public async Task<IActionResult> UpdateAttendance(string username, Guid attendanceId)
+    {
+        return Ok();
+    }
+}
