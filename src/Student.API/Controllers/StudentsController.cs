f46c392ba865eb86f65b6acb4ada238540c0aac8
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-14 03:51:44 -0700

    All end points for students controller were added

--- src/Student.API/Controllers/StudentsController.cs
+++ src/Student.API/Controllers/StudentsController.cs
@@ -1,11 +1,56 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Student.API.Controllers
+namespace Student.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentsController : ControllerBase
+    [HttpGet]
+    public async Task<IActionResult> GetStudents()
     {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddStudent()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{username}")]
+    public async Task<IActionResult> UpdateStudent(string username)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetStudentByUsername(string username)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{username}")]
+    public async Task<IActionResult> DeleteStudent(string username)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{username}/attendance")]
+    public async Task<IActionResult> GetAttendances(string username)
+    {
+        return Ok();
+    }
+
+    [HttpPost("{username}/attendance")]
+    public async Task<IActionResult> AddAttendance()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{username}/attendance/{attendanceId}")]
+    public async Task<IActionResult> UpdateAttendance(string username, Guid attendanceId)
+    {
+        return Ok();
     }
 }
