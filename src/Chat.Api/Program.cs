1a1e41facbfd3221170919b9ba588db1e4f4de80
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-15 17:40:17 +0500

    called service extension method on program.cs

--- src/Chat.Api/Program.cs
+++ src/Chat.Api/Program.cs
@@ -1,9 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Chat.Api.Extensions;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddChatServices(builder.Configuration);
 
 var app = builder.Build();
 
