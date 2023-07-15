27a8dd9d20bab47dff231abc0bf48cb2eb86a49b
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-15 17:36:23 +0500

    added chatdbcontext, extension methods for dbcontext, added dockerfile, changed dokcer-compose, added db configurations to appsettings.json

---
+++ src/Chat.Api/Context/ChatDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Chat.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Api.Context;
+
+public class ChatDbContext : DbContext
+{
+    public DbSet<Entities.Chat> Chats { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/Chat.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Chat.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddChatServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddChatDbContext(configuration);
+    }
+}
