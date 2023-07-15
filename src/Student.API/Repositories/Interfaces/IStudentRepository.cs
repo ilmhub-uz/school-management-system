03681dfe87f04488ff1be8f3c23656f6fcb241bd
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-15 16:29:34 +0500

    IStudentRepository added

---
+++ src/Student.API/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Student.API.Repositories.Interfaces;
+using Student = Entities.Student;
+ public interface IStudentRepository
+    {
+        public Task<List<Student>> GetStudents();
+        public Task AddStudent(Student student);
+        public Task UpdateStudent(Student student);
+        public Task DeleteStudent(Student student);
+        public Task<Student> GetStudentById(Guid studentId);
+        public Task<Student> GetStudentByUserName(string username);
+ }
+
