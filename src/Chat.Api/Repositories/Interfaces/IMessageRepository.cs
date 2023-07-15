24114452b452a1d95532da0648431f7d5232659d
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-15 23:20:03 +0500

    added message repository, added to service collection extension method

---
+++ src/Chat.Api/Repositories/Interfaces/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Api.Entities;
+
+namespace Chat.Api.Repositories.Interfaces;
+
+public interface IMessageRepository
+{
+    public Task AddMessage(Message message);
+    public Task UpdateMessage(Message message);
+    public Task DeleteMessage(Message message);
+    public Task<Message?> GetMessageById(int id);
+    public Task<List<Message>?> GetMessagesByChatId(int chatId);
+    public Task<List<Message>?> GetMessagesByUserId(Guid userId);
+}
