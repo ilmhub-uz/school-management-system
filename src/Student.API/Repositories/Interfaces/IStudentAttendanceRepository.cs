3296d677350fbf6eddcebdf238f6a08d46cea494
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-15 17:42:41 +0500

    Add IStudentAttendanceRepository interface

---
+++ src/Student.API/Repositories/Interfaces/IStudentAttendanceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Student.API.Entities;
+
+namespace Student.API.Repositories.Interfaces
+{
+    public interface IStudentAttendanceRepository
+    {
+        Task<List<StudentAttendance>> GetStudentAttendance(string studentUsername);
+        Task AddStudentAttendance(StudentAttendance studentAttendance, string studentUsername);
+        Task UpdateStudentAttendance(StudentAttendance studentAttendance, string studentUsername);
+        
+    }
+}
