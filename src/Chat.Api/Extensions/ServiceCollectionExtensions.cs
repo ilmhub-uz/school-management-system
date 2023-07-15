9cbe45fd3127476c88d40e21a78265f6432e4162
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-15 23:04:28 +0500

    added chatrepository

--- src/Chat.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Chat.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,14 @@
-﻿namespace Chat.Api.Extensions;
+﻿using Chat.Api.Repositories;
+using Chat.Api.Repositories.Interfaces;
+
+namespace Chat.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
     public static void AddChatServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddChatDbContext(configuration);
+
+        services.AddScoped<IChatRepository, ChatRepository>();
     }
 }
