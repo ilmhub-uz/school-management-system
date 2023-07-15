51dae8c81df4d47d3fee6b356e7e2e1a00a85fb3
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-15 13:10:14 +0500

    add student configuration

---
+++ src/Student.API/Context/StudentConfigurations/StudentConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student.API.Entities;
+
+namespace Student.API.Context.StudentConfigurations;
+
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Student> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
