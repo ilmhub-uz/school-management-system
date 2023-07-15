d703fd1101b9c23d2e539e95b8c41e4c3767ed6c
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-15 13:11:15 +0500

    update

--- src/Student.API/Context/StudentConfigurations/StudentConfiguration.cs
+++ src/Student.API/Context/StudentConfigurations/StudentConfiguration.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Student.API.Entities;
 
 namespace Student.API.Context.StudentConfigurations;
 
 public class StudentConfiguration : IEntityTypeConfiguration<Student>
 {
-    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Student> builder)
+
+    public void Configure(EntityTypeBuilder<Student> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("students");
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.UserName).IsRequired(true);
+        builder.Property(s => s.PhoneNumber).IsRequired(true);
+        builder.Property(s => s.CreateAt).IsRequired(true);
+        builder.Property(s => s.Status).IsRequired(true);
     }
 }
