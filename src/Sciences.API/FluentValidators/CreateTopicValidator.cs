74f1992bba4b1d24e0d82c9f2f748d91aea7c422
Jasurbek Isaev <jasurbekisaev1@gmail.com>
2023-07-15 15:22:24 +0500

    CreateTopic validator is added. Commentlar ochilishi kerak negadur using deb ishlatayotkanimda ochib ketib qolyapti saqlasam

---
+++ src/Sciences.API/FluentValidators/CreateTopicValidator.cs
@@ -0,0 +1,22 @@
+﻿//using FluentValidation;
+//using Sciences.API.Models.TopicModels;
+
+//namespace Sciences.API.FluentValidators;
+
+
+
+public class CreateTopicValidator : AbstractValidator<CreateTopicModel>
+{
+    public CreateTopicValidator()
+    {
+        RuleFor(t => t.Name)
+            .NotNull()
+            .Length(3, 50)
+            .WithMessage("Topic Name should have at least 3 letters and 50 letters maximum");
+
+        RuleFor(t => t.Title)
+            .NotNull()
+            .WithMessage("it can not be empty");
+
+    }
+}
