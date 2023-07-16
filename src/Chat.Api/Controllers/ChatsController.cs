e0566161bfde3684efbc396e296b848fc305f693
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-16 19:08:08 +0500

    added chats_controller, changed managers. None validations

---
+++ src/Chat.Api/Controllers/ChatsController.cs
@@ -0,0 +1,56 @@
+﻿using Chat.Api.Managers.Interfaces;
+using Chat.Api.Models.ChatModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ChatsController : ControllerBase
+{
+    private readonly IChatManager _chatManager;
+
+    public ChatsController(IChatManager chatManager)
+    {
+        _chatManager = chatManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateChat([FromBody] CreateChatModel createChatModel)
+    {
+        //davomi bor
+        await _chatManager.CreateChat(createChatModel);
+        return Ok();
+    }
+
+    [HttpGet("{chatId}")]
+    public async Task<IActionResult> GetChat(int chatId)
+    {
+        var chatModel = await _chatManager.GetChat(chatId);
+        return Ok(chatModel);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetChats()
+    {
+        //davomi bor
+        var chats = await _chatManager.GetChats();
+        return Ok(chats);
+    }
+
+    [HttpPut("chatId")]
+    public async Task<IActionResult> UpdateChats(int chatId, UpdateChatModel updateChat)
+    {
+        //davomi bor
+        await _chatManager.UpdateChat(updateChat, chatId);
+        return Ok();
+    }
+
+    [HttpDelete("chatId")]
+    public async Task<IActionResult> DeleteChat(int chatId)
+    {
+        await _chatManager.DeleteChat(chatId);
+        return Ok();
+    }
+}
