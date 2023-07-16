e0fd0439ad21758c322c5659429168b4bbbfb872
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-16 20:30:58 +0500

    jamolmeyliyev/update-istudent-science-repository

--- src/Student.API/Repositories/Interfaces/IStudentScienceRepository.cs
+++ src/Student.API/Repositories/Interfaces/IStudentScienceRepository.cs
@@ -6,8 +6,8 @@ namespace Student.API.Repositories.Interfaces
     {
         Task<List<StudentScience>> GetStudentSciencesAsync(string studentUsername);
         Task AddStudentScienceAsync(string studentUsername, StudentScience science);
-        Task GetStudentScienceByScienceIdASync(string studentUsername,int scienceId);
-        Task UpdateStudentScienceByScienceIdASync(string studentUsername, int scienceId);
+        Task GetStudentScienceByScienceIdAsync(string studentUsername,int scienceId);
+        Task UpdateStudentScienceByScienceIdAsync(string studentUsername, int scienceId);
         Task DeleteStudentScienceByScienceIdAsync(string studentUsername, int scienceId);
 
 
