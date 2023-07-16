c81b7ab36571502bdec567727d413243a10fc41c
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-16 12:37:38 +0500

    jamolmeyliyev/change-function-name

--- src/Student.API/Repositories/Interfaces/IStudentRepository.cs
+++ src/Student.API/Repositories/Interfaces/IStudentRepository.cs
@@ -2,11 +2,11 @@
 using Student = Entities.Student;
  public interface IStudentRepository
     {
-        public Task<List<Student>> GetStudents();
-        public Task AddStudent(Student student);
-        public Task UpdateStudent(Student student);
-        public Task DeleteStudent(Student student);
-        public Task<Student> GetStudentById(Guid studentId);
-        public Task<Student> GetStudentByUserName(string username);
+        public Task<List<Student>> GetStudentsAsync();
+        public Task AddStudentAsync(Student student);
+        public Task UpdateStudentAsync(Student student);
+        public Task DeleteStudentAsync(Student student);
+        public Task<Student> GetStudentByIdAsync(Guid studentId);
+        public Task<Student> GetStudentByUserNameAsync(string username);
  }
 
