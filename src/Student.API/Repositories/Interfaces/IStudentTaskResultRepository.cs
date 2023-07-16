378a3c05dd61a229ff024db55ceea269fdae5645
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-16 20:39:37 +0500

    jamolmeyliyev/add interface for StudentTaskResult

---
+++ src/Student.API/Repositories/Interfaces/IStudentTaskResultRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using Student.API.Entities;
+using System.Threading.Tasks;
+
+namespace Student.API.Repositories.Interfaces
+{
+    public interface IStudentTaskResultRepository
+    {
+        Task<List<StudentTaskResult>> GetTaskResultsAsync(string studenUsername);
+        Task AddTaskResultAsync(string studentUsername,StudentTaskResult result);
+        Task GetTaskResultByTaskIdAsync(string studentUsername, Guid TaskId);
+        Task UpdateTaskResultByTaskIdAsync(string studentUsername, Guid TaskId);
+        Task DeleteTaskResultByTaskIdAsync(string studentUsername, Guid TaskId);
+
+    }
+     //GET students/{username}/ task - results
+     //POST students/{username}/ task - results
+     //GET students/{username}/ task - results /{ task_id}
+     //PUT students/{username}/ task - results /{ task_id}
+     //DELETE students/{username}/ task - results /{ task_id}
+}
