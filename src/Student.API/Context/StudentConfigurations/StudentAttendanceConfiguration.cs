67d25f8f6384b7420e30065d1f30827de18416d6
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-16 14:26:55 +0500

    jamolmeyliyev/change StudentAttendance configuration 2

---
+++ src/Student.API/Context/StudentConfigurations/StudentAttendanceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Student.API.Entities;
+
+namespace Student.API.Context.StudentConfigurations
+{
+    public class StudentAttendanceConfiguration : IEntityTypeConfiguration<StudentAttendance>
+    {
+        public void Configure(EntityTypeBuilder<StudentAttendance> builder)
+        {
+            builder.ToTable("student_attendances");
+            builder.Property(s => s.StudentId).IsRequired(true);
+            builder.Property(s => s.TopicId).IsRequired(true);
+       }
+    }
+}
