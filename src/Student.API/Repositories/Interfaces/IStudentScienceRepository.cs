acb6f6f304d2dfc74c035d8879056581673d5c1b
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-16 12:53:55 +0500

    jamolmeyliyev/add interface for StudentScienceRepository

---
+++ src/Student.API/Repositories/Interfaces/IStudentScienceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Student.API.Entities;
+
+namespace Student.API.Repositories.Interfaces
+{
+    public interface IStudentScienceRepository
+    {
+        public Task<List<StudentScience>> GetStudentSciences(string studentUsername);
+        public Task AddStudentScience(string studentUsername, StudentScience science);
+        public Task GetStudentScienceByScienceId(string studentUsername,int scienceId);
+        public Task UpdateStudentScienceByScienceId(string studentUsername, int scienceId);
+        public Task DeleteStudentScienceByScienceId(string studentUsername, int scienceId);
+
+
+
+
+
+    }
+}
