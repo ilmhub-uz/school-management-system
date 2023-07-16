7d2aadfa2f3dd132f6b9b494dc628d1eadf1da5c
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-16 12:11:48 +0500

    add new project gateway api

---
+++ src/Gateway.Api/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.Cache.Middleware;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
+
+app.Run();
