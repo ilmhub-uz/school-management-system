4c31c78fe475f203e23ca10dc0d7a91f9115958a
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 03:37:37 +0500

    jamolmeyliyev/add student science validator

---
+++ src/Student.API/FluentValidators/UpdateStudentScienceValidor.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Student.API.Models.StudentScienceModels;
+
+namespace Student.API.FluentValidators
+{
+    public class UpdateStudentScienceValidor:AbstractValidator<UpdateStudentScienceModel>
+    {
+        public UpdateStudentScienceValidor()
+        {
+            RuleFor(s => s.ScienceId).NotNull();
+            RuleFor(s => s.StudentId).NotNull();
+            RuleFor(s => s.Status).NotNull();
+            RuleFor(s => s.CreateAt).NotNull();
+        }
+    }
+}
