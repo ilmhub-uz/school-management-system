2d94689976ad64873a61935a92cdfe4cd04d297d
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 03:40:30 +0500

    jamolmeyliyev/add student science exceptions

---
+++ src/Student.API/Exceptions/AddStudentScienceValidationIsNotValid.cs
@@ -0,0 +1,7 @@
+﻿namespace Student.API.Exceptions
+{
+    public class AddStudentScienceValidationIsNotValid:Exception
+    {
+        public AddStudentScienceValidationIsNotValid(string message) : base(message) { }
+    }
+}
