0f2236a7f824e209b07579710af3d4a02a1b2608
Husayn Toshturdiyev <husainjon9@gmail.com>
2023-07-17 18:54:05 +0500

    app.Environment.IsDevelopment

--- src/Chat.Api/Program.cs
+++ src/Chat.Api/Program.cs
@@ -14,11 +14,10 @@ builder.Services.AddChatServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
