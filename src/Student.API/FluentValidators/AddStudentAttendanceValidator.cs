5388c7872a3a33d9337608e45f65bbe6241366f4
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 00:09:46 +0500

    jamolmeyliyev/add-student-attendance-validator

---
+++ src/Student.API/FluentValidators/AddStudentAttendanceValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Student.API.Models.StudentAttendanceModels;
+
+namespace Student.API.FluentValidators
+{
+    public class AddStudentAttendanceValidator: AbstractValidator<AddStudentAttendanceModel>
+    {
+        public AddStudentAttendanceValidator() 
+        {
+            RuleFor(r => r.TopicId).NotNull();
+            RuleFor(r => r.StudentId).NotNull(); 
+        }
+
+    }
+}
