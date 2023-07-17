17946336bc1b039b5ce0dc6ec760f185ce23fd4f
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-17 21:21:53 +0500

    TopicManager added

--- src/Sciences.API/Managers/TopicManager.cs
+++ src/Sciences.API/Managers/TopicManager.cs
@@ -1,4 +1,8 @@
-﻿using Sciences.API.Models.TopicModels;
+﻿using Mapster;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Sciences.API.Entities;
+using Sciences.API.Models.TopicModels;
+using Sciences.API.ParseHelper;
 using Sciences.API.Repositories.Interfaces;
 
 namespace Sciences.API.Managers;
@@ -15,10 +19,46 @@ public class TopicManager
     public async Task<List<TopicModel>> GetTopicsAsync()
     {
         var topics = await _topicRepository.GetTopics();
-        if (topics == null || topics.Count == 0)
+        return ParseService.ParseToTopicList(topics);
+    }
+
+    public async Task<TopicModel> AddTopicAsync(CreateTopicModel topic,Guid scienceId)
+    {
+        var model=new Topic()
         {
-            return new List<TopicModel>();
-        }
+            Name = topic.Name,
+            Title = topic.Title,
+            ScienceId = scienceId,
+            Id = Guid.NewGuid(),
+            CreatedAt = DateTime.UtcNow,
+        };
+        await _topicRepository.AddTopic(model);
+        return ParseService.ParseToTopicModel(model);
+    }
+
+    public async Task<TopicModel> UpdateTopicAsync(Guid topicId, UpdateTopicModel topic)
+    {
+        var model = await _topicRepository.GetTopicById(topicId);
+
+        model.Name=topic.Name;
+        model.Title=topic.Title;
+        await _topicRepository.UpdateTopic(model);
+        return ParseService.ParseToTopicModel(model);
 
     }
+
+    public async Task DelateTopicAsync(Guid topicId)
+    {
+        var model = await _topicRepository.GetTopicById(topicId);
+        await _topicRepository.DeleteTopic(model);
+
+    }
+
+    public async Task<TopicModel> GetTopicByIdAsync(Guid topicId)
+    {
+        var topic = await _topicRepository.GetTopicById(topicId);
+        return ParseService.ParseToTopicModel(topic);
+    }
+
+
 }
