0b59d5835f22a88f26441061bf22b0457199475a
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 00:01:56 +0500

    jamolmeyliyev/add-student-attendance-model

---
+++ src/Student.API/Models/StudentAttendanceModels/AddStudentAttendanceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Student.API.Models.StudentAttendanceModels
+{
+    public class AddStudentAttendanceModel
+    {
+        public required Guid TopicId { get; set; }
+        public required Guid StudentId { get; set; }
+    }
+}
