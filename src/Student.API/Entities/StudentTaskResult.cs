685af50ff366d3829a29d03587de3a1cca232d01
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 14:21:53 +0500

    jamolmeyliyev/add student task result manager and other change

--- src/Student.API/Entities/StudentTaskResult.cs
+++ src/Student.API/Entities/StudentTaskResult.cs
@@ -5,7 +5,7 @@ public class StudentTaskResult
 	public Guid TaskId { get; set; }
 	public Guid StudentId { get; set; }
 	public Student? Student { get; set; }
-	public required DateTime CreateDate { get; set; } = DateTime.UtcNow;
-	public DateTime? UpdateDate { get; set; }
+	public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime? UpdatedAt { get; set; }
 	public required string Content { get; set; }
 }
