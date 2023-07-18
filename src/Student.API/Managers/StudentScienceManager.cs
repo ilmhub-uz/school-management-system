f51765dc51211d4e73d25ef58606a9c6963a0c4b
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 15:17:41 +0500

    extra

--- src/Student.API/Managers/StudentScienceManager.cs
+++ src/Student.API/Managers/StudentScienceManager.cs
@@ -6,89 +6,88 @@ using Student.API.Managers.Interfaces;
 using Student.API.Models.StudentScienceModels;
 using Student.API.Repositories.Interfaces;
 
-namespace Student.API.Managers
+namespace Student.API.Managers;
+
+public class StudentScienceManager:IStudentScienceManager
 {
-    public class StudentScienceManager:IStudentScienceManager
+    private readonly IStudentScienceRepository _studentScienceRepos;
+    public StudentScienceManager(IStudentScienceRepository studentScienceRepos)
     {
-        private readonly IStudentScienceRepository _studentScienceRepos;
-        public StudentScienceManager(IStudentScienceRepository studentScienceRepos)
-        {
-            _studentScienceRepos = studentScienceRepos;
-        }
-
-        public async Task<List<StudentScienceModel>> GetStudentSciencesAsync()
-        {
-            var studentScience = await _studentScienceRepos.GetStudentSciencesAsync();
-
-            return  ToStudentScienceModels(studentScience);
-        }
-        public async Task<StudentScienceModel> AddStudentScienceAsync(Guid studentId)
-        {
-            
-            var studentScience = new StudentScience()
-            {
-                ScienceId = Guid.NewGuid(),
-                StudentId = studentId,
-                CreatedAt = DateTime.UtcNow
-            };
-            await _studentScienceRepos.AddStudentScienceAsync(studentScience);
-            return ToStudentScienceModel(studentScience);
-        }
-
-        public async Task<StudentScienceModel> GetStudentScienceByScienceIdAsync(Guid studentId,Guid scienceId)
-        {
-            var studentScience = await _studentScienceRepos.GetStudentScienceByScienceIdAsync(scienceId, studentId);
-
-            return ToStudentScienceModel(studentScience);
-        }
-
-        public async Task UpdateStudentScienceAsync(Guid studentId, Guid scienceId,UpdateStudentScienceModel model)
-        {
-            var validator = new UpdateStudentScienceValidator();
-            var result = await validator.ValidateAsync(model);
-            if (!result.IsValid)
-            {
-                throw new UpdateStudentScienceValidationIsNotValid("Invalid update input try again");
-            };
-            var studentScience = await _studentScienceRepos.GetStudentScienceByScienceIdAsync(scienceId, studentId);
-
-            studentScience.Status = model.Status;
-            studentScience.UpdatedAt = DateTime.UtcNow;
-
-            await _studentScienceRepos.UpdateStudentScienceAsync(studentScience);
-        }
-
-        private StudentScienceModel ToStudentScienceModel(StudentScience studentScience)
-        {
-            var model = new StudentScienceModel()
-            {
-                ScienceId = studentScience.ScienceId,
-                StudentId = studentScience.StudentId,
-                CreatedAt = studentScience.CreatedAt,
-            };
-            if(studentScience.Status == Status.Created)
-            {
-                model.UpdatedAt = null;
-            }
-            else
-            {
-                model.UpdatedAt = studentScience.UpdatedAt;
-            }
-            return model;
-        }
-        private List<StudentScienceModel> ToStudentScienceModels(List<StudentScience> studentSciences)
-        {
-            if(studentSciences == null || studentSciences.Count == 0)
-            {
-                return new List<StudentScienceModel>();
-            }
-            var models = new List<StudentScienceModel>();
-            foreach(var studentScience in studentSciences)
-            {
-                models.Add(ToStudentScienceModel(studentScience));
-            }
-            return models;
-        }
-        
+        _studentScienceRepos = studentScienceRepos;
     }
+
+    public async Task<List<StudentScienceModel>> GetStudentSciencesAsync()
+    {
+        var studentScience = await _studentScienceRepos.GetStudentSciencesAsync();
+
+        return  ToStudentScienceModels(studentScience);
+    }
+    public async Task<StudentScienceModel> AddStudentScienceAsync(Guid studentId)
+    {
+        
+        var studentScience = new StudentScience()
+        {
+            ScienceId = Guid.NewGuid(),
+            StudentId = studentId,
+            CreatedAt = DateTime.UtcNow
+        };
+        await _studentScienceRepos.AddStudentScienceAsync(studentScience);
+        return ToStudentScienceModel(studentScience);
+    }
+
+    public async Task<StudentScienceModel> GetStudentScienceByScienceIdAsync(Guid studentId,Guid scienceId)
+    {
+        var studentScience = await _studentScienceRepos.GetStudentScienceByScienceIdAsync(scienceId, studentId);
+
+        return ToStudentScienceModel(studentScience);
+    }
+
+    public async Task UpdateStudentScienceAsync(Guid studentId, Guid scienceId,UpdateStudentScienceModel model)
+    {
+        var validator = new UpdateStudentScienceValidator();
+        var result = await validator.ValidateAsync(model);
+        if (!result.IsValid)
+        {
+            throw new UpdateStudentScienceValidationIsNotValid("Invalid update input try again");
+        };
+        var studentScience = await _studentScienceRepos.GetStudentScienceByScienceIdAsync(scienceId, studentId);
+
+        studentScience.Status = model.Status;
+        studentScience.UpdatedAt = DateTime.UtcNow;
+
+        await _studentScienceRepos.UpdateStudentScienceAsync(studentScience);
+    }
+
+    private StudentScienceModel ToStudentScienceModel(StudentScience studentScience)
+    {
+        var model = new StudentScienceModel()
+        {
+            ScienceId = studentScience.ScienceId,
+            StudentId = studentScience.StudentId,
+            CreatedAt = studentScience.CreatedAt,
+        };
+        if(studentScience.Status == Status.Created)
+        {
+            model.UpdatedAt = null;
+        }
+        else
+        {
+            model.UpdatedAt = studentScience.UpdatedAt;
+        }
+        return model;
+    }
+    private List<StudentScienceModel> ToStudentScienceModels(List<StudentScience> studentSciences)
+    {
+        if(studentSciences == null || studentSciences.Count == 0)
+        {
+            return new List<StudentScienceModel>();
+        }
+        var models = new List<StudentScienceModel>();
+        foreach(var studentScience in studentSciences)
+        {
+            models.Add(ToStudentScienceModel(studentScience));
+        }
+        return models;
+    }
+    
 }
