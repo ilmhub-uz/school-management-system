7c162dde0f860d0313777307e83704cab4a5d0ba
Professional <Professional@USER-PC>
2023-07-18 17:00:06 +0500

    Student-task-result-model

---
+++ src/Student.API/Models/StudentTaskResults/AddStudentTaskResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Student.API.Models.StudentTaskResults
+{
+	public class AddStudentTaskResultModel
+	{
+		public Guid TaskId { get; set; }
+		public Guid StudentId { get; set; }
+		public required string Context { get; set; }
+	}
+}
