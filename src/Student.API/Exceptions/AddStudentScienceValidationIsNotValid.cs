b0bbd3a5b6cb01a79a8319d8fc6f60aa25ef25d5
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 15:09:23 +0500

    jamolmeyliyev/remove some  validator,model and exception

--- src/Student.API/Exceptions/AddStudentScienceValidationIsNotValid.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Student.API.Exceptions
-{
-    public class AddStudentScienceValidationIsNotValid:Exception
-    {
-        public AddStudentScienceValidationIsNotValid(string message) : base(message) { }
-    }
-}
