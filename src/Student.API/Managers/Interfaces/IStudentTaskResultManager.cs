af37b8d2012d594f65a686ca7bb6f87e0f7ec375
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-18 14:54:32 +0500

    jamolmeyliyev/add-interface-for-managers

---
+++ src/Student.API/Managers/Interfaces/IStudentTaskResultManager.cs
@@ -0,0 +1,23 @@
+﻿using Student.API.Entities;
+using Student.API.Exceptions;
+using Student.API.FluentValidators;
+using Student.API.Models.StudentTaskResults;
+
+namespace Student.API.Managers.Interfaces
+{
+    public interface IStudentTaskResultManager
+    {
+        Task<List<StudentTaskResultModel>> GetStudentTaskResultsAsync();
+
+
+        Task<StudentTaskResultModel> AddStudentTaskResultAsync(Guid studentId, AddStudentTaskResultModel model);
+
+
+        Task UpdateStudentTaskResultAsync(Guid studentId, Guid taskId, UpdateStudentTaskResultModel model);
+
+
+        Task DeleteStudentTaskResultAsync(Guid studentId, Guid taskId);
+        
+
+    }
+}
