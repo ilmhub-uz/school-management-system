d5de67951cf917ecb7cc0fd385aee164c89a5da1
Professional <Professional@USER-PC>
2023-07-18 17:02:20 +0500

    adding student task result model

---
+++ src/Student.API/Models/StudentTaskResults/StudentTaskResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Student.API.Models.StudentTaskResults
+{
+	public class StudentTaskResultModel
+	{
+		public Guid TaskId { get; set; }
+		public Guid StudentId { get; set; }
+		public DateTime? UpdateAt { get; set; }
+		public required string Content { get; set; }
+	}
+}
