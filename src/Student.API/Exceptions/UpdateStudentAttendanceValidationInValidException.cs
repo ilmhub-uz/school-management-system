81dd06440901f1ac62ddacb936895f45625d0421
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-20 15:54:03 +0500

    add-try-cache-student-attendance-controller

---
+++ src/Student.API/Exceptions/UpdateStudentAttendanceValidationInValidException.cs
@@ -0,0 +1,6 @@
+﻿namespace Student.API.Exceptions;
+
+public class UpdateStudentAttendanceValidationInValid : Exception
+{
+    public UpdateStudentAttendanceValidationInValid(string message) : base($"Update StudentAttendanceValidation InValid!: {message}") { }
+}
