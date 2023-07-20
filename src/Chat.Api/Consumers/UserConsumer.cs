edb8c79671776583190e611b67948526ecaf2b02
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-07-20 19:04:18 +0500

    added to container some classes

--- src/Chat.Api/Consumers/UserConsumer.cs
+++ src/Chat.Api/Consumers/UserConsumer.cs
@@ -1,14 +1,15 @@
 ﻿using Chat.Api.Entities;
 using Chat.Api.Managers;
+using Chat.Api.Managers.Interfaces;
 using MassTransit;
 
 namespace Chat.Api.Consumers;
 
 public class UserConsumer : IConsumer<User>
 {
-    private readonly UserManager _userManager;
+    private readonly IUserManager _userManager;
 
-    public UserConsumer(UserManager userManager)
+    public UserConsumer(IUserManager userManager)
     {
         _userManager = userManager;
     }
