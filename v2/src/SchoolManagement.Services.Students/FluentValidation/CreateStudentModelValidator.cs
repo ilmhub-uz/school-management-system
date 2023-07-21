808be031a686f44a1b0994a8ff56cb2edb6d06af
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-07-21 20:21:11 +0500

    adding CreateStudentModelValidator

--- v2/src/SchoolManagement.Services.Students/FluentValidation/CreateStudentModelValidator.cs
+++ v2/src/SchoolManagement.Services.Students/FluentValidation/CreateStudentModelValidator.cs
@@ -1,6 +1,33 @@
-﻿namespace SchoolManagement.Services.Students.FluentValidation
+﻿using System.Data;
+using FluentValidation;
+using SchoolManagement.Services.Students.Models;
+
+namespace SchoolManagement.Services.Students.FluentValidation
 {
-    public class CreateStudentModelValidation
+    public class CreateStudentModelValidation: AbstractValidator<CreateStudentModel>
     {
+
+        public CreateStudentModelValidation()
+        {
+            RuleFor(r => r.FirstName)
+                .MinimumLength(5)
+                .MaximumLength(50);
+
+            RuleFor(r => r.LastName)
+                .MinimumLength(5)
+                .MaximumLength(50);
+
+            RuleFor(r => r.MiddleName)
+                .MinimumLength(5)
+                .MaximumLength(50);
+
+            RuleFor(r => r.Username)
+                .MinimumLength(5)
+                .MaximumLength(50);
+
+            RuleFor(r => r.PhoneNumber)
+                .MinimumLength(8)
+                .MaximumLength(12);
+        }
     }
 }
