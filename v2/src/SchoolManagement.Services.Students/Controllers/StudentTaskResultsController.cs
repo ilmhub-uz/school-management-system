77e3770c3445d6da056adfcd7f3707b70b5139f1
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-25 15:37:22 +0500

    add StudentTaskResultsController

---
+++ v2/src/SchoolManagement.Services.Students/Controllers/StudentTaskResultsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.Services.Students.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentTaskResultsController : ControllerBase
+    {
+    }
+}
