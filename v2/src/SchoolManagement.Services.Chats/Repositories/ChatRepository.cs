d658b80295886f6543b834adeea4fead9d58e59a
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-26 14:56:05 +0500

    add Chat Repositroy

--- v2/src/SchoolManagement.Services.Chats/Repositories/ChatRepository.cs
+++ v2/src/SchoolManagement.Services.Chats/Repositories/ChatRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace SchoolManagement.Services.Chats.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Services.Chats.Context;
+using SchoolManagement.Services.Chats.Entities;
+
+namespace SchoolManagement.Services.Chats.Repositories;
+
+public class ChatRepository : IChatRepository
 {
-    public class ChatRepository
+    private readonly ChatsDbContext _context;
+    public ChatRepository(ChatsDbContext context)
     {
+        _context = context;
+    }
+    public async Task<List<Chat>?> GetChats()
+    {
+        var chats = await _context.Chats.ToListAsync();
+        if(chats is null || chats.Count==0)
+        {
+            return new List<Chat>();
+        }
+        return chats;
+    }
+    public async Task<Chat?> GetChatById(Gui chatId)
+    {
+        var chat = await _context.Chats.FirstOrDefaultAsync(u => u.Id == chatId);
+        return chat;
+    }
+
+    public async Task AddChat(Chat chat)
+    {
+        _context.Chats.Add(chat);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteChat(Chat chat)
+    {
+        _context.Chats.Remove(chat);
+        await _context.SaveChangesAsync();
+    }
+    public async Task UpdateChat(Chat chat)
+    {
+        _context.Chats.Update(chat);
+        await _context.SaveChangesAsync();
     }
 }
