fab9c59a33a5dec9a36670ab5bca1e63621161b5
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-28 06:20:14 -0700

    Routes which belong to ocelot.json were moved to diffirent json files

--- v2/src/SchoolManagement.AdminAPI/Program.cs
+++ v2/src/SchoolManagement.AdminAPI/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddSwaggerGenJwt();
 builder.Services.AddJwt(builder.Configuration);
 
 builder.Configuration.AddJsonFile("ocelot.json", false, false);
+builder.Configuration.AddJsonFile("account.json", false, false);
+builder.Configuration.AddJsonFile("science.json", false, false);
+builder.Configuration.AddJsonFile("student.json", false, false);
 builder.Services.AddOcelot(builder.Configuration);
 
 var configuration = builder.Configuration;
