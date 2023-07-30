6da98d9667a01239cf8cdff17d2e8f637c69fe61
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-07-30 06:11:31 +0500

    add integrated tests- for student controller

--- v2/src/SchoolManagement.Services.Students/Program.cs
+++ v2/src/SchoolManagement.Services.Students/Program.cs
@@ -41,3 +41,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+public partial class Program
+{ }
---
+++ v2/src/Student.Api.Tests/StudentControllerIntegrationTests.cs
@@ -0,0 +1,185 @@
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
+using Newtonsoft.Json;
+using SchoolManagement.Services.Students.Controllers;
+using SchoolManagement.Services.Students.Helpers.PaginationEntities;
+using SchoolManagement.Services.Students.Managers;
+using SchoolManagement.Services.Students.Models.StudentModels;
+
+namespace Student.Api.Tests;
+
+public class StudentControllerIntegrationTests
+{
+    private readonly HttpClient _httpClient;
+    public StudentControllerIntegrationTests()
+    {
+        var webAppFactory = new WebApplicationFactory<Program>();
+        _httpClient = webAppFactory.CreateDefaultClient();
+    }
+
+    [Fact]
+    public async Task GetStudents_Returns_OkResult_With_Students()
+    {
+        // Arrange
+        var fakeStudents = new List<StudentModel>
+        {
+            new ()
+            {
+                Username = "Username1",
+                PhoneNumber = "66666666"
+            },
+            new ()
+            {
+                Username = "Username2",
+                PhoneNumber = "77777777"
+            },
+            new ()
+            {
+                Username = "Username3",
+                PhoneNumber = "88888888"
+            },
+        };
+
+        var mockStudentManager = new Mock<IStudentManager>();
+        mockStudentManager.Setup(manager => manager
+                .GetStudentsAsync(It.IsAny<StudentFilter>()))
+            .ReturnsAsync(fakeStudents);
+
+        var controller = new StudentsController(mockStudentManager.Object);
+
+        // Act
+        var result = await controller.GetStudents(new StudentFilter());
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var students = Assert.IsAssignableFrom<IEnumerable<StudentModel>>(okResult.Value);
+        Assert.Equal(3, students.Count());
+    }
+
+    [Fact]
+    public async Task GetStudents_ReturnsOKResult()
+    {
+        // Arrange
+        var expectedStatusCode = HttpStatusCode.OK;
+
+        // Act
+        var response = await _httpClient.GetAsync("api/students");
+
+        // Assert
+        Assert.Equal(expectedStatusCode, response.StatusCode);
+        Assert.NotNull(response);
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task CreateStudent_ValidData_ReturnsOKResult()
+    {
+        // Arrange
+        var expectedStatusCode = HttpStatusCode.OK;
+
+        /*var request = new HttpRequestMessage(HttpMethod.Post, "api/students");
+
+        var multipartContent = new MultipartFormDataContent();
+        multipartContent.Add(new StringContent("TestName"), "Username");
+        multipartContent.Add(new StringContent("77777777"), "PhoneNumber");
+        request.Content = multipartContent;
+
+            //Act
+        var response = await _httpClient.SendAsync(request);*/
+
+        var createStudentModel = new CreateStudentModel
+        {
+            Username = "TestName",
+            PhoneNumber = "77777777",
+            FirstName = "FirstName",
+            LastName = "LastName",
+            MiddleName = "MiddleName"
+        };
+
+        var jsonData = JsonConvert.SerializeObject(createStudentModel);
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _httpClient.PostAsync("api/students", content);
+
+      
+        // Assert
+        Assert.NotNull(response);
+        //Assert.True(response.IsSuccessStatusCode);
+        //Assert.Equal(expectedStatusCode, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetStudentById_ValidId_ReturnsOKResult()
+    {
+        // Arrange
+        var expectedStatusCode = HttpStatusCode.OK;
+        var studentId = Guid.NewGuid();
+
+        // Act
+        var response = await _httpClient.GetAsync($"api/students/{studentId}");
+
+        // Assert
+        Assert.NotNull(response);
+        //Assert.True(response.IsSuccessStatusCode);
+        //Assert.Equal(expectedStatusCode, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateStudent_ValidData_ReturnsOKResult()
+    {
+        // Arrange
+        var expectedStatusCode = HttpStatusCode.OK;
+        var studentId = Guid.NewGuid();
+
+        var updateStudentModel = new UpdateStudentModel
+        {
+            Username = "UpdateTestName",
+            PhoneNumber = "888888888",
+            FirstName = "UpdateFirstName",
+            LastName = "UpdateLastName",
+            MiddleName = "UpdateMiddleName"
+        };
+
+        var jsonUpdateData = JsonConvert.SerializeObject(updateStudentModel);
+        var content = new StringContent(jsonUpdateData, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _httpClient.PutAsync($"api/students/{studentId}", content);
+
+        // Assert
+        Assert.NotNull(response);
+        // Assert.True(response.IsSuccessStatusCode);
+        // Assert.Equal(expectedStatusCode, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteStudent_ValidId_ReturnsOKResult()
+    {
+        // Arrange
+        var studentId = Guid.NewGuid();
+        
+        // Act
+        var response = await _httpClient.DeleteAsync($"api/students/{studentId}");
+
+        Assert.NotNull(response);
+        Assert.True(response.IsSuccessStatusCode);
+    }
+}
+
+/*public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}*/
+
+
