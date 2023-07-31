5ac69dde564838caf2bcc0fa9228689c76891447
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-31 16:27:17 +0500

    jamolmeyliyev/add-chats-controller

---
+++ v2/src/SchoolManagement.Services.Chats/Controllers/ChatsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Services.Chats.Managers;
+using SchoolManagement.Services.Chats.Models;
+
+namespace SchoolManagement.Services.Chats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+        private readonly IChatManager _chatManager;
+        public ChatsController(IChatManager chatManager)
+        {
+            _chatManager = chatManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetChats()
+        {
+            var chats = await _chatManager.GetAllChats();
+            return Ok(chats);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChat([FromBody] CreateChatModel model)
+        {
+            try
+            {
+                var chat = await _chatManager.AddChat(model);
+                return Ok(chat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPut("{chatId}")]
+        public async Task<IActionResult> UpdateChat(ulong chatId, UpdateChatModel model)
+        {
+            try
+            {
+                await _chatManager.UpdateChat(chatId, model);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpDelete("{chatId}")]
+        public async Task<IActionResult> DeleteChat(ulong chatId)
+        {
+
+            try
+            {
+                await _chatManager.Delete(chatId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{chatId}")]
+        public async Task<IActionResult> GetByChatId(ulong chatId)
+        {
+            try
+            {
+               var chat = await _chatManager.GetById(chatId);
+                return Ok(chat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
