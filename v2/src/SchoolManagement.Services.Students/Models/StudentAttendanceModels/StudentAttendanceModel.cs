acc5549afa8f5502af475af291151af8f1cb64b1
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-01 12:29:45 +0500

    jamolmeyliyev/add StudentAttendance Controller Integration Tests

--- v2/src/SchoolManagement.Services.Students/Models/StudentAttendanceModels/StudentAttendanceModel.cs
+++ v2/src/SchoolManagement.Services.Students/Models/StudentAttendanceModels/StudentAttendanceModel.cs
@@ -1,9 +1,12 @@
-﻿namespace SchoolManagement.Services.Students.Models.StudentAttendanceModels;
+﻿using SchoolManagement.Services.Students.Models.StudentModels;
+
+namespace SchoolManagement.Services.Students.Models.StudentAttendanceModels;
 
 public class StudentAttendanceModel 
 { 
 
 public required Guid TopicId { get; set; }
 public required Guid StudentId { get; set; }
+public StudentModel? Student { get; set; }
 public bool Attend { get; set; }
 }
